0c79c32f1462b90aed548869f30e15125e18ae30
Jain4556 <sunil925business@gmail.com>
2026-02-15 15:36:34 +0530

    nextjs_created

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+export default function ProfilePage({params}: any) {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p className="text-4xl">ProfilePage
+            <span className="p-2 ml-2 rounded  bg-white-500 text-black">{params.id}</span>
+            </p>
+
+        </div>
+    )
+}
