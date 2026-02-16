d760a18ecb551b15413b065957ab1efd499572fa
Jain4556 <sunil925business@gmail.com>
2026-02-16 20:19:22 +0530

    login_route_added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,61 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { set } from "mongoose";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 
 export default function LoginPage() {
+    const router = useRouter()
     const [user, setUser] = React.useState({
         email: "",
-        password: "", 
+        password: "",
     })
 
-    const onLogin = async () => {
+    const [buttonDisabled, setButtonDisabled] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
 
+
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user)
+            console.log("Login success", response.data);
+
+            toast.success("Login success")
+
+            router.push("/profile")
+
+
+
+
+
+        } catch (error: any) {
+            console.log("login failed", error.message);
+            toast.error(error.message)
+
+        }
+        finally {
+            setLoading(false)
+        }
     }
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+
+            setButtonDisabled(false)
+        }
+        else {
+            setButtonDisabled(true);
+        }
+    }, [user])
 
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login</h1>
+            <h1>{loading ? "Processing" : "Login"}</h1>
 
             <hr />
 
@@ -42,8 +80,8 @@ export default function LoginPage() {
             />
 
             <button
-            onClick={onLogin}
-                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
+                onClick={onLogin}
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No login" : "login"}</button>
             <Link href="/signup">visit SignUp page</Link>
 
         </div>
