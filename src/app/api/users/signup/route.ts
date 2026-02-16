1a0ef3037b8214e933399c90b59352d99b6f1d42
Jain4556 <sunil925business@gmail.com>
2026-02-16 18:47:43 +0530

    route_added

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,55 @@
+import { connect } from "@/dbConfig/dbConfig"
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
+import { error } from "console"
+
+
+
+
+
+
+connect()
+
+
+export async function POST(request: NextRequest,) {
+    try {
+        const reqBody = await request.json()
+        const { username, email, password } = reqBody
+
+        console.log(reqBody);
+
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return NextResponse.json({ error: "User already exist" }, { status: 400 })
+        }
+
+
+        //    hash password
+        const salt = await bcrypt.genSalt(10)
+
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+
+        // new user
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        const savedUser = await newUser.save()
+        console.log(savedUser);
+
+        return NextResponse.json({
+            message: "user created successfully", 
+            success: true, 
+            savedUser
+        })
+
+    } catch (error: any) {
+        
+    }
+
+}
