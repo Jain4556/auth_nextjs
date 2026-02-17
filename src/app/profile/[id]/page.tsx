d29fabd3e496482e88563d06ae457d742bcd9e98
Jain4556 <sunil925business@gmail.com>
2026-02-17 19:35:24 +0530

    profile_update_err

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,10 @@
-export default function UserProfile({params}: any) {
+export default async function UserProfile({params}: any) {
+    const {id} = await params;
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            <p className="text-4xl ">Profile Page {params.id}</p>
+            <p className="text-4xl ">Profile Page {id}</p>
 
         </div>
     )
