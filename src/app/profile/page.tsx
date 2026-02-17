ce4f5cbab84f5e10bedc106994b50e1d29843666
Jain4556 <sunil925business@gmail.com>
2026-02-17 19:58:38 +0530

    logout_added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,38 @@
-export default function ProfilePage({params}: any) {
+"use client"
+import axios from "axios"
+import Link from "next/link"
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+export default function ProfilePage() {
+    const router = useRouter();
+
+
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout")
+            toast.success("Logout Successful")
+            router.push("/login")
+        } catch (error: any) {
+            console.log(error.message);
+
+            toast.error(error.message)
+        }
+
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            <p className="text-4xl">ProfilePage
-            <span className="p-2 ml-2 rounded  bg-white-500 text-black">{params.id}</span>
-            </p>
+            <p className="text-4xl">ProfilePage</p>
+
+            <hr />
+            <button
+                onClick={logout}
+                className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >Logout</button>
+
 
         </div>
     )
